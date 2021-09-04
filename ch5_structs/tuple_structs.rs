526ef07f8ecd998f49f75319885b7f8d023e625a
jcawl <jcawl123@gmail.com>
2021-09-04 13:01:02 -0400

    tuple structs

--- ch5_structs/tuple_structs.rs
+++ ch5_structs/tuple_structs.rs
@@ -1,9 +1,12 @@
 fn main() {
+    //init two tuple structs
+    //dont have to specify name
     struct Color(i32, i32, i32);
     struct Point(i32, i32, i32);
 
     let black = Color(55, 0, 0);
     let origin = Point(0, 0, 0);
 
+    //use "." followed by the index to access a value
     println!("{}", black.0);
 }
